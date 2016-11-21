9ac1d1879328fd93b3457f4f5d63e5c32284ef13
Sushil Choudhary <sushil.choudhary@cloudsherpas.com>
2016-11-21 16:56:49 +0000

    Initial files

---
+++ src/main/java/hmrc/shopping/basket/checkout/App.java
@@ -0,0 +1,13 @@
+package hmrc.shopping.basket.checkout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
