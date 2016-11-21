07eb670eadc74016d8724683589f41744236fe8b
Sushil Choudhary <sushil.choudhary@cloudsherpas.com>
2016-11-21 17:19:27 +0000

    test for mix of fruits in basket to get actual price

--- src/main/java/com/hmrc/shopping/basket/checkout/ShoppingPriceCalculator.java
+++ src/main/java/com/hmrc/shopping/basket/checkout/ShoppingPriceCalculator.java
@@ -8,21 +8,19 @@ import rx.Observable;
 
 public class ShoppingPriceCalculator {
 
-
 	private Observable<String> basketObservable;
 	private Observable<Double> totalPriceObservable;
-	
+
 	public ShoppingPriceCalculator(List<String> basketItems) {
 		basketObservable = Observable.from(basketItems);
 		totalPriceObservable = Observable.merge(getApplePrice(), getOrangePrice()).reduce(0.0, Double::sum);
-		
+
 	}
 
 	public Observable<? extends Object> totalActualPrice() {
 		// TODO Auto-generated method stub
 		return totalPriceObservable;
 	}
-
 
 	private Observable<Double> getApplePrice() {
 		return basketObservable.filter(ItemEnum.APPLE.name()::equals).map((eachOffer) -> ItemEnum.APPLE.getPrice());
--- src/test/java/com/hmrc/shopping/basket/checkout/ShoppingBasketCheckoutTest.java
+++ src/test/java/com/hmrc/shopping/basket/checkout/ShoppingBasketCheckoutTest.java
@@ -2,11 +2,14 @@ package com.hmrc.shopping.basket.checkout;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+
+import com.hmrc.shoppping.model.ItemEnum;
 
 import rx.observables.BlockingObservable;
 
@@ -18,6 +21,38 @@ public class ShoppingBasketCheckoutTest {
 		List<String> basket = Arrays.asList();
 		ShoppingPriceCalculator calc = new ShoppingPriceCalculator(basket);
 		assertEquals(BlockingObservable.from(calc.totalActualPrice()).last(), Double.valueOf(0.0));
+	}
+
+
+	@Test
+	public void testActualPriceOfBasketFullOfApples() {
+		List<String> basketOfApples = Arrays.asList(ItemEnum.APPLE.name(), ItemEnum.APPLE.name());
+		ShoppingPriceCalculator calc = new ShoppingPriceCalculator(basketOfApples);
+		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(1.20));
+	}
+
+	@Test
+	public void testActualPriceOfBasketFullOfOranges() {
+		List<String> basketOfOranges = Arrays.asList(ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(),
+				ItemEnum.ORANGE.name());
+		ShoppingPriceCalculator calc = new ShoppingPriceCalculator(basketOfOranges);
+		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(0.75));
+	}
+
+	@Test
+	public void testActualPriceOfBasketOfMultipleItems() {
+		List<String> basket = Arrays.asList(ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(), ItemEnum.APPLE.name(),
+				ItemEnum.APPLE.name());
+		ShoppingPriceCalculator calc = new ShoppingPriceCalculator(basket);
+		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(1.70));
+	}
+
+	@Test
+	public void testActualPriceOfBasketOfMultipleMixedItems() {
+		List<String> basket = Arrays.asList(ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(),
+				ItemEnum.ORANGE.name(), ItemEnum.APPLE.name(), ItemEnum.APPLE.name(), ItemEnum.APPLE.name());
+		ShoppingPriceCalculator calc = new ShoppingPriceCalculator(basket);
+		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(2.80));
 	}
 
 }
